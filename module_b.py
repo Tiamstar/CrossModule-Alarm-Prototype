cc04576db59ddd0aa6f7c9ce3fd40c216520eb8b
李非凡 <150361482+Tiamstar@users.noreply.github.com>
2025-10-27 10:29:45 +0800

    Add files via upload

---
+++ module_b.py
@@ -0,0 +1,11 @@
+from module_a import get_divisor
+
+
+def compute_ratio(y: int) -> float:
+    divisor = get_divisor(y)
+# 当 divisor == 0 时，会触发 ZeroDivisionError
+    return 10 / divisor
+
+
+if __name__ == "__main__":
+   print(compute_ratio(0))
